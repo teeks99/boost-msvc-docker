e04e586bcd0de3609d396021d4ffea806a623896
Thomas Kent <tom@teeks99.com>
2020-12-29 19:35:15 -0600

    Full Build/Push with date

--- build_win_images.bat
+++ build_win_images.bat
@@ -1,6 +1,12 @@
 
-for %%v in ( 14.1 14.2 ) do (
+set datestr=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+echo %datestr% > lastBuildDate.txt
+
+for %%v in ( 14.0 14.1 14.2 ) do (
   pushd %%v
-  docker build -t teeks99/boost-msvc:%%v -m 2g .
+  docker build -t teeks99/boost-msvc:%%v_%datestr% -m 2g .
+  docker tag teeks99/boost-msvc:%%v_%datestr% teeks99/boost-msvc:%%v
   popd
 )
+
+docker tag teeks99/boost-msvc:14.2 teeks99/boost-msvc:latest
--- push_win_images.bat
+++ push_win_images.bat
@@ -1,3 +1,12 @@
-for %%v in ( 14.1 14.2 ) do (
+set datestr=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+if exist lastBuildDate.txt (
+  set /p datestr=<lastBuildDate.txt
+)
+
+for %%v in ( 14.0 14.1 14.2 ) do (
+  docker push teeks99/boost-msvc:%%v_%datestr%
   docker push teeks99/boost-msvc:%%v
 )
+
+docker push teeks99/boost-msvc:latest
